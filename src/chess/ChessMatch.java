71fa997f0c488f6da1588fd46fc7e107e6c73430
Leandro <leanddrosouzasilva@gmail.com>
2022-10-23 11:16:28 -0300

    Add chessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,15 @@
 package chess;
 
 import boardGame.Board;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
-	
+	// Cria a partida de xadrez
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -17,5 +20,15 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece,  new ChessPosition(column, row).toPosition());
+	}
+	
+	// adiciona as peças no tabuleiro
+	private void initialSetup() {
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('h', 1, new King(board, Color.BLACK));
 	}
 }
