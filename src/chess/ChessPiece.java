91c3f38cfa1f6d651e14cebffed61fa5e6d884f8
Leandro <leanddrosouzasilva@gmail.com>
2022-10-24 20:07:48 -0300

    check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -16,6 +16,11 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	// testando se a cor da peça é adversaria ou não
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
